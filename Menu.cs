7e0d1697a9926d7d6a52718e9af992a626237a7e
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2024-08-19 17:24:59 -0500

    Initial commit

---
+++ Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2DVisualStudioSemana1
+{
+    internal class Menu
+    {
+        public void Execute()
+        {
+            Test();
+        }
+
+        private void Test()
+        {
+            Console.WriteLine("Introduce tu nombre");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hola {name}");
+            int age;
+            bool continueFlag = true;
+
+            while (continueFlag)
+            {
+                Console.WriteLine("Introduce tu edad");
+                age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Tu edad es {age}");
+                if (age < 0)
+                {
+                    Console.WriteLine("No puedes tener edad negativa");
+                }
+                else if (age <= 18)
+                {
+                    continueFlag = false;
+                    Console.WriteLine("Eres menor de edad, no tomes alcohol");
+                }
+                else
+                {
+                    continueFlag = false;
+                    Console.WriteLine("Toma todo lo que quieras");
+                }
+            }
+        }
+    }
+}
